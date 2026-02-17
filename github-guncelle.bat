40dcac6ad746b8330b6e9f59101fb95dcc172e12
yvzsltn1-blip <yvzsltn1@gmail.com>
2026-02-18 02:39:09 +0300

    Guncelleme: Çar 18.02.2026  2:39:05,45

--- github-guncelle.bat
+++ github-guncelle.bat
@@ -1,12 +1,22 @@
 @echo off
-set /p commit_mesaji="Degisiklik notunu yazin: "
+set "tarih=%date% %time%"
+
+:: Kullanıcıdan mesaj girişi bekler
+set /p user_msg="Degisiklik notunu girin (Bos birakirsan tarih yazilir): "
+
+:: Eğer kullanıcı bir şey yazmadıysa tarih değişkenini ata
+if "%user_msg%"=="" (
+    set "final_msg=Guncelleme: %tarih%"
+) else (
+    set "final_msg=%user_msg%"
+)
 
 echo.
 echo [1/3] Dosyalar hazirlaniyor...
 git add .
 
-echo [2/3] Degisiklikler kaydediliyor...
-git commit -m "%commit_mesaji%"
+echo [2/3] Kaydediliyor: %final_msg%
+git commit -m "%final_msg%"
 
 echo [3/3] GitHub'a gonderiliyor...
 git push
